e3fff50ca45c182f250ffb36ab2800e0cbeca1bf
김정현 <invent819@naver.com>
2025-07-04 21:11:21 +0900

    fix: 게시글, 댓글 날짜 포맷팅 수정

--- lib/util/util.dart
+++ lib/util/util.dart
@@ -1,7 +1,6 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:intl/intl.dart';
 // import 'package:timezone/browser.dart';
 
@@ -135,16 +134,12 @@ class DateTimeUtil {
 
   static String formatStringDateTime(String dateTime) {
     return DateFormat('yyyy년 MM월 dd일 HH:mm', 'ko')
-        .format(DateTime.parse(dateTime));
+        .format(DateTime.parse(dateTime).toLocal());
   }
 
   static String formatDate(DateTime dateTime) {
     final formatter = DateFormat("MM월 dd일");
     return formatter.format(dateTime);
-  }
-
-  static DateTime getyMd({required DateTime dateTime}) {
-    return DateTime(dateTime.year, dateTime.month, dateTime.day);
   }
 
   static String parseMd({required DateTime dateTime}) {
@@ -185,7 +180,6 @@ class DateTimeUtil {
       final year = _estimateYear(month, day);
 
       return DateTime(year, month, day, actualHour, minute);
-
     } catch (e) {
       print('DateTime 파싱 오류: $e');
       print('입력 값 - date: "$date", time: "$time"');
@@ -219,36 +213,18 @@ class DateTimeUtil {
     }
   }
 
-
   /// 오전/오후를 24시간 형식으로 변환
   static int _convertTo24Hour(String period, int hour) {
     if (period == '오전') {
       return hour == 12 ? 0 : hour;
-    } else { // 오후
+    } else {
+      // 오후
       return hour == 12 ? 12 : hour + 12;
     }
   }
 
-
-
-  static int getMonth({required String dateTime}) {
-    return int.parse(DateFormat('MM').format(DateTime.parse(dateTime)));
-  }
-
-  static String getDateRange({required String start, required String end}) {
-    final formatStart = DateFormat('MM월 dd일').format(DateTime.parse(start));
-    final formatEnd = DateFormat('MM월 dd일').format(DateTime.parse(end));
-
-    return '$formatStart ~ $formatEnd';
-  }
-
   static String getDate({required DateTime dateTime, String sep = ' / '}) {
     DateFormat dateFormat = DateFormat('yyyy${sep}MM${sep}dd');
-    return dateFormat.format(dateTime);
-  }
-
-  static String getDateTime({required DateTime dateTime}) {
-    DateFormat dateFormat = DateFormat('MM.dd aa hh:mm', 'ko');
     return dateFormat.format(dateTime);
   }
 
@@ -279,29 +255,8 @@ class NumberUtil {
 }
 
 class ValidRegExp {
-  static bool gameTitle(String input) {
-    RegExp regex = RegExp(
-        r'^[가-힣a-zA-Z\(\)\{\}\[\]0-9][가-힣a-zA-Z\(\)\{\}\[\]0-9\s\:]{4,64}$');
-    return regex.hasMatch(input);
-  }
-
-  static bool gameDate(String input) {
-    RegExp regex = RegExp(r'^\d{4}-\d{2}-\d{2}$');
-    return regex.hasMatch(input);
-  }
-
-  static bool gameTime(String input) {
-    RegExp regex = RegExp(r'^\d{2}:\d{2}$');
-    return regex.hasMatch(input);
-  }
-
   static bool gameAddress(String input) {
     RegExp regex = RegExp(r'^[가-힣a-zA-Z0-9][가-힣a-zA-Z0-9\s\-\(\)\.]{2,128}$');
-    return regex.hasMatch(input);
-  }
-
-  static bool gameAddressDetail(String input) {
-    RegExp regex = RegExp(r'^[가-힣a-zA-Z0-9][가-힣a-zA-Z0-9\s\-\(\)\.]{2,64}$');
     return regex.hasMatch(input);
   }
 
@@ -318,11 +273,6 @@ class ValidRegExp {
   static bool userPassword(String input) {
     RegExp regex = RegExp(
         r'^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*()])[\w\d!@#$%^&*()]{8,20}$');
-    return regex.hasMatch(input);
-  }
-
-  static bool userPhoneNumber(String input) {
-    RegExp regex = RegExp(r'^01(\d){9}$');
     return regex.hasMatch(input);
   }
 
@@ -344,39 +294,6 @@ class ValidRegExp {
       }
     }
     return true;
-  }
-}
-
-class TextStyleUtil {
-  static TextStyle getLabelTextStyle() {
-    return TextStyle(
-      color: const Color(0xFF999999),
-      fontSize: 14.sp,
-      fontFamily: 'Pretendard',
-      fontWeight: FontWeight.w400,
-      height: 1.4,
-      letterSpacing: -0.25.sp,
-    );
-  }
-
-  static TextStyle getTextStyle() {
-    return TextStyle(
-      color: Colors.black,
-      fontSize: 14.sp,
-      fontFamily: 'Pretendard',
-      fontWeight: FontWeight.w500,
-      letterSpacing: -0.25.sp,
-    );
-  }
-
-  static TextStyle getHintTextStyle() {
-    return TextStyle(
-      color: const Color(0xFF969696),
-      fontSize: 14.sp,
-      fontFamily: 'Pretendard',
-      fontWeight: FontWeight.w400,
-      letterSpacing: -0.25.sp,
-    );
   }
 }
 
