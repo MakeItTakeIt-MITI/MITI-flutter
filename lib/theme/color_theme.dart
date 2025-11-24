dc326dbd59255add045d324335929a44bfaac2c9
김정현 <invent819@naver.com>
2025-11-24 19:16:02 +0900

    design(*): 컬러 색상 추가

--- lib/theme/color_theme.dart
+++ lib/theme/color_theme.dart
@@ -19,3 +19,40 @@ class MITIColor {
   static const Color gray900 = Color(0xFF171717);
   static const Color black = Color(0xFF000000);
 }
+
+class V2MITIColor {
+  static const Color primary1 = Color(0xFFD1F8F8);
+  static const Color primary2 = Color(0xFFA3F1F2);
+  static const Color primary3 = Color(0xFF75EAEB);
+  static const Color primary4 = Color(0xFF47E3E5);
+  static const Color primary5 = Color(0xFF1ADCDF);
+  static const Color primary6 = Color(0xFF14B0B2);
+  static const Color primary7 = Color(0xFF0F8485);
+  static const Color primary8 = Color(0xFF0A5859);
+  static const Color primary9 = Color(0xFF052C2C);
+
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color gray1 = Color(0xFFEBEBEB);
+  static const Color gray2 = Color(0xFFD6D6D6);
+  static const Color gray3 = Color(0xFFC2C2C2);
+  static const Color gray4 = Color(0xFFADADAD);
+  static const Color gray5 = Color(0xFF999999);
+  static const Color gray6 = Color(0xFF858585);
+  static const Color gray7 = Color(0xFF707070);
+  static const Color gray8 = Color(0xFF5C5C5C);
+  static const Color gray9 = Color(0xFF474747);
+  static const Color gray10 = Color(0xFF333333);
+  static const Color gray11 = Color(0xFF292929);
+  static const Color gray12 = Color(0xFF141414);
+  static const Color black = Color(0xFF000000);
+
+  static const Color red1 = Color(0xFFFAD8D7);
+  static const Color red2 = Color(0xFFF5B1B0);
+  static const Color red3 = Color(0xFFF18B89);
+  static const Color red4 = Color(0xFFEC6462);
+  static const Color red5 = Color(0xFFE83E3B);
+  static const Color red6 = Color(0xFFB9312F);
+  static const Color red7 = Color(0xFF8B2523);
+  static const Color red8 = Color(0xFF5C1817);
+  static const Color red9 = Color(0xFF451211);
+}
